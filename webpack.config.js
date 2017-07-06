21faa255cbba10a2449d376c745fb30a18aabb87
bhcbhc <995265694@qq.com>
2017-07-06 18:00:48 +0800

    webpack config re-write

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const OpenBrowserPlugin = require('open-browser-webpack-plugin');
 
-const extractTextCss = new ExtractTextPlugin('app.css');
+const extractCommonCss = new ExtractTextPlugin('[name]_common.css');
+const extratPrivateCss = new ExtractTextPlugin('[name]_private.css');
 
 module.exports = {
     entry: {
-        app: './src/js/index.js',
+        app: './src/index.js',
         bundle: [
             'react', 'react-dom'
         ]
@@ -42,20 +43,61 @@ module.exports = {
             use: [
                 'file-loader'
             ]
-        }, {
-            test: /\.css$/,
-            use: ExtractTextPlugin.extract({fallback: 'style-loader', use: 'css-loader'}),
-            exclude: /node_modules/
-        },
-            //共有样式
+        }, //共有样式
             {
                 test: /\.scss/,
                 include: path.resolve(__dirname, 'src/styles'),
-                use: ExtractTextPlugin.extract({
-                    fallback:'style-loader',
-                    use:['css-loader','scss-loader']
+                use: extractCommonCss.extract({
+                    fallback: 'style-loader',
+                    use: [{
+                        loader: 'css-loader', options: {
+                            minimize: true,
+                            importLoaders: 1
+                        }
+                    }, {
+                        /*                        loader: 'postcss-loader', options: {
+                         plugins: (loader) => [require('precss')(), require('autoprefixer')(), require('rucksack-css')()]
+                         }*/
+                        loader: 'sass-loader'
+                    }]
                 })
-            }]
+            },//私有样式
+            {
+                test: /\.scss/,
+                include: path.join(__dirname, 'src/js'),
+                use: extratPrivateCss.extract({
+                    fallback: 'style-loader',
+                    use: [{
+                        loader: 'css-loader', options: {
+                            modules: true, //启动css modules
+                            minimize: true, //压缩css代码
+                            importLoaders: 1, //在css-loader 之前loader的数目
+                            localIdentName: '[local]___[hash:base64:5]' //设置css-modules模式下的local类名的变量
+                        }
+                    }, {
+              /*          loader: 'postcss-loader', options: {
+                            plugins: (loader) => [require('precss'), require('autoprefixer'), require('rucksack-css')]
+                        }*/
+                         loader:'sass-loader'
+                    }]
+                })
+            },
+            {
+                test: /\.(otf|eot|svg|ttf|woff|woff2).*$/,
+                use: [{
+                    loader: 'url-loader', options: {
+                        limit: 8192
+                    }
+                }]
+            }, {
+                test: /\.(gif|jpe?g|png|ico)$/,
+                use: [{
+                    loader: 'url-loader', options: {
+                        limit: 8192
+                    }
+                }]
+            }
+        ]
     },
     plugins: [
         // new webpack.optimize.UglifyJsPlugin({minimize: true}),
@@ -67,13 +109,15 @@ module.exports = {
                 removeComments: true,
                 collapseWhitespace: true
             },
+            chunks: ['bundle', 'app'],
             hash: true,
             inject: 'body'
         }),
         new OpenBrowserPlugin({
             url: 'http://localhost:8888'
         }),
-        extractTextCss
+        extractCommonCss,
+        extratPrivateCss
     ],
     devServer: {
         contentBase: './',
